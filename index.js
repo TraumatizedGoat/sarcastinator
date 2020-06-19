007436d8325a63c6f57e567f9d164e2fd01fd923
TraumatizedGoat <62394513+TraumatizedGoat@users.noreply.github.com>
2020-06-18 23:11:44 -0600

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+function sarcastinator(words){
+    
+    var output = "";
+
+    for(i=0; i < words.length; i++){
+        if(i % 2 != 0){
+            output += words[i].toUpperCase();
+        }
+        else {
+            output += words[i].toLowerCase();
+         }   
+    }
+    return output;
+}
+// var words = document.querySelector("#yourText").value;
+// var shitPost = sarcastinator(words);
+
+$("button").click(function(){
+    var words = document.querySelector("#yourText").value;
+    var shitPost = sarcastinator(words);
+    $("h4").text(shitPost);
+    
+});
